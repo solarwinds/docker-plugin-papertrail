821045e31160ef458eda789db71ed7e308ab9db1
Girish Ranganathan <girish.ranganathan@solarwinds.com>
2017-11-16 17:26:30 -0500

    Added some level of resilience with BoltDB persistence

--- papertrail_log_shipper.go
+++ papertrail_log_shipper.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 	"strings"
@@ -11,6 +12,7 @@ import (
 
 	"log/syslog"
 
+	"github.com/boltdb/bolt"
 	"github.com/docker/docker/daemon/logger"
 
 	log "github.com/sirupsen/logrus"
@@ -18,7 +20,10 @@ import (
 	"github.com/pkg/errors"
 )
 
-const name = "Docker Paper Trail Logger"
+const (
+	name       = "Docker Paper Trail Logger"
+	bucketName = "docker"
+)
 
 type PaperTrailLogger struct {
 	paperTrailURL string
@@ -33,6 +38,8 @@ type PaperTrailLogger struct {
 
 	readers map[*logger.LogWatcher]struct{} // map for the active log followers
 	mu      sync.Mutex
+
+	db *bolt.DB
 }
 
 func NewPaperTrailLogger(logCtx logger.Info) (*PaperTrailLogger, error) {
@@ -66,9 +73,16 @@ func NewPaperTrailLogger(logCtx logger.Info) (*PaperTrailLogger, error) {
 		return nil, err
 	}
 
+	db, err := bolt.Open(fmt.Sprintf("/tmp/%s.db", logCtx.ContainerID), 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		err := errors.Errorf("Unable to open a database for papertrail log processing.")
+		log.Error(err)
+		return nil, err
+	}
+
 	log.Infof("Creating a new paper trail logger for url: %s", paperTrailURL)
 
-	return &PaperTrailLogger{
+	p := &PaperTrailLogger{
 		paperTrailURL: paperTrailURL,
 		token:         paperTrailToken,
 		httpClient:    client,
@@ -77,42 +91,94 @@ func NewPaperTrailLogger(logCtx logger.Info) (*PaperTrailLogger, error) {
 		containerCreatedTime: logCtx.ContainerCreated,
 
 		readers: map[*logger.LogWatcher]struct{}{},
-	}, nil
+
+		db: db,
+	}
+	go p.flushLogs()
+	return p, nil
 }
 
 func (p *PaperTrailLogger) Log(msg *logger.Message) error {
 	if len(msg.Line) > 0 {
-		writer, err := syslog.Dial("udp", p.paperTrailURL, syslog.LOG_EMERG|syslog.LOG_KERN, p.containerID)
+		err := p.db.Update(func(tx *bolt.Tx) error {
+			buc, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+			if err != nil {
+				return fmt.Errorf("Unable to create bucket error: %v", err)
+			}
+			err = buc.Put([]byte(fmt.Sprintf("%d", time.Now().UnixNano())), msg.Line)
+			return err
+		})
 		if err != nil {
-			e := errors.Wrap(err, "failed to dial syslog")
-			log.Error(e)
-			return e
-		}
-
-		if msg.Source == "stderr" || msg.Err != nil {
-			err = writer.Err(string(msg.Line))
-		} else {
-			err = writer.Info(string(msg.Line))
-		}
-		if err != nil {
-			e := errors.Wrap(err, "failed to write log msg")
+			e := errors.Wrap(err, fmt.Sprintf("Unable to store the log for further processing: %s", string(msg.Line)))
 			log.Error(e)
 			return e
 		}
 	}
 	return nil
 }
+func (p *PaperTrailLogger) sendLogs(data []byte) error {
+	var err error
+	if p.writer == nil {
+		p.writer, err = syslog.Dial("udp", p.paperTrailURL, syslog.LOG_EMERG|syslog.LOG_KERN, p.containerID)
+		if err != nil {
+			e := errors.Wrap(err, "failed to dial syslog")
+			log.Error(e)
+			return e
+		}
+	}
+	err = p.writer.Info(string(data))
+	if err != nil {
+		e := errors.Wrap(err, "failed to send log msg to papertrail")
+		log.Error(e)
+		return e
+	}
+	return nil
+}
+
+// This should be run in a routine
+func (p *PaperTrailLogger) flushLogs() {
+	var err error
+	for p.db != nil {
+		err = p.db.Update(func(tx *bolt.Tx) error {
+			// Assume bucket exists and has keys
+			b, err := tx.CreateBucketIfNotExists([]byte(bucketName))
+			if err != nil {
+				return fmt.Errorf("Unable to create bucket error: %v", err)
+			}
+
+			b.ForEach(func(k, v []byte) error {
+				err = p.sendLogs(v)
+				if err == nil {
+					err = b.Delete(k)
+				}
+				return err
+			})
+			return nil
+		})
+		if err != nil {
+			e := errors.Wrap(err, "Error reading the data in the DB and shipping logs")
+			log.Error(e)
+		}
+		time.Sleep(time.Second)
+	}
+}
+
 func (p *PaperTrailLogger) Name() string {
 	return name
 }
 func (p *PaperTrailLogger) Close() error {
+	var err error
 	if p.writer != nil {
-		err := p.writer.Close()
+		err = p.writer.Close()
 		if err != nil {
 			e := errors.Wrap(err, "failed to close papertrail logger")
 			log.Error(e)
 			return e
 		}
 	}
-	return nil
+	if p.db != nil {
+		err = p.db.Close()
+		p.db = nil // this will stop the flush loop
+	}
+	return err
 }
